64b8d5ca1b30a24a3f67bf47566dd84386935b5d
calvinUBB <calvinsan123@gmail.com>
2020-04-01 00:24:47 +0700

    menambahkan fitur logout

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,7 +5,11 @@ class Admin extends CI_Controller {
 
 	public function index()
 	{
-		
+		$data['title'] = 'Dashboard';
+		$data['user'] = $this->db->get_where('user', [
+			'email' => $this->session->userdata('email')])->row_array();
+
+		$this->load->view('admin/index', $data);
 	}
 
 }
